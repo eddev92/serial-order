a0efb10ce40a850253c74b19313731373fcdae78
edward <edwarllanca@hotmail.com>
2017-01-27 12:32:44 -0500

    add grunt tasks connect and watch,

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,17 @@
-/*
- * Este archivo muestra una configuración para iniciar el servidor y
- * levantar al plugin watch.
+/*  Archivo de configuracion de grunt 
+ *  y a su vez indexa diversas tareas
  *
- * Note: Configuración básica para los primeros entregables
- * se trata de no tener muchos archivos, o instrucciones sueltas
- * en la aplicación.
+ * @Edward Llanca Haro (edwarllanca@hotmail.com)
  *
- * @author Edward Llanca Haro (edwarllanca@hotmail.com)
  */
+
 'use strict';
 
 module.exports = function (grunt) {
-
-  grunt.initConfig({
-    connect: { // Conexión al servidor local
-      server: {
-        options: {
-          port: 9000,
-          base: 'app/',
-          keepalive: true
-        }
-      }
+  require('load-grunt-tasks')(grunt);
+  require('load-grunt-config')(grunt, {
+    data: {
+      pkg: grunt.file.readJSON('package.json')
     }
   });
-
-  // Carga de paquequetes instalados en 'node_modules'
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  
-  // Registro de tareas para levantar procesos de grunt.
-  // Si se desea agregar mas tareas deben ser agregadas tambien
-  // en esta linea.
-  grunt.registerTask('default', ['connect']);
-
 };
---
+++ grunt/config.js
@@ -0,0 +1,23 @@
+/* Variables para configurar el grunt */
+var vendor = 'bower_components';
+
+module.exports = {
+	app: 'app',
+	port: 9000,
+	vendor: '/bower_components',
+	appComponents: '/components',
+	appCore: '/core',
+	appViews: '/views',
+	jsDependencies: [
+		vendor + '/jquery/dist/jquery.min.js',
+		vendor + '/angular/angular.min.js',
+		vendor + '/bootstrap/dist/js/bootstrap.min.js',
+		vendor + '/angular-ui-router/release/angular-ui-router.min.js',
+		vendor + '/angular-animate/angular-animate.min.js',
+		vendor + '/angular-material/angular-material.min.js',	
+		vendor + '/angular-bootstrap/ui-bootstrap-tpls.min.js'
+	],
+	cssDependencies: [
+		'css/dist/app.css'
+	]
+}
